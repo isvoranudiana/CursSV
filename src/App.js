1347032eb67995d049af4032f9e293521e8bb79d
isvoranudiana <104450967+isvoranudiana@users.noreply.github.com>
2022-05-15 18:39:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import Exercitiul1 from "./componente/examples/exercitiu1";
+import Exercitiul2 from "./componente/examples/exercitiu2";
+import Exercitiul3 from "./componente/examples/exercitiul3";
+import Example4 from "./componente/examples/example4"; 
+import Example5 from "./componente/examples/example5";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Exercitiul1 />
+        <br />
+        <Exercitiul2 />
+        <br />
+        <Exercitiul3 />
+        <br />
+        <Example4 />
+        <br />
+        <Example5 />
+      </div>
+      <footer> Curs 3 </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componente/examples/exercitiu1.js
@@ -0,0 +1,15 @@
+import Bottle from "../bottle";
+
+const Exercitiul1 = () => {
+  return (
+    <div className="example">
+      Exercitiu 1
+      <br/>
+      <formular />
+      <br />
+      <Bottle />
+    </div>
+  );
+};
+
+export default Exercitiul1;
